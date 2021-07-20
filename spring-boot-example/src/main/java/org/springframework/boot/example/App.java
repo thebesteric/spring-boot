52ede1d72af899e26c603e197a3c517d9c454a7d
eric <whatisjava@hotmail.com>
2021-07-20 15:24:16 +0800

    init

---
+++ spring-boot-example/src/main/java/org/springframework/boot/example/App.java
@@ -0,0 +1,11 @@
+package org.springframework.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
